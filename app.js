09645390bd86ae7b2e269e27ff2daa040b68d9e8
t2000chris <t2000chris@gmail.com>
2020-07-09 23:32:33 +0800

    Add CPU loading

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const app = new express();
 
 app.get('/', (req, res) => res.send('Do not browse root!!'));
 require('./routes/versionRoutes')(app, config);
-require('./routes/getPiRoutes')(app, config);
+require('./routes/cpuLoadRoutes')(app, config);
 
 app.listen(3000, () => console.log('Server up!'));
